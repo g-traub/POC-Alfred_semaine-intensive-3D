b72aab209f2ba29aab004235f7f95e5c51dae76d
gtrb <guillaume.trb@hotmail.com>
2019-04-15 18:02:36 +0200

    first test

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-const vision = require('@google-cloud/vision');
+async function quickstart() {
+  // Imports the Google Cloud client library
+  const vision = require('@google-cloud/vision');
 
+  // Creates a client
+  const client = new vision.ImageAnnotatorClient();
+
+  // Performs label detection on the image file
+  const [result] = await client.labelDetection('./assets/cat-217679.jpg');
+  const labels = result.labelAnnotations;
+  console.log('Labels:');
+  labels.forEach(label => console.log(label.description));
+}
+
+quickstart();
